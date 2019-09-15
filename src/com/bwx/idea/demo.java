233f3e7bf6490535d00cca263d1f0ef0d0dd4bb6
Baolvlv <baolvlv@gmail.com>
2019-09-15 21:48:23 +0800

    first commit

---
+++ src/com/bwx/idea/demo.java
@@ -0,0 +1,31 @@
+package com.bwx.idea;
+
+import java.util.Map;
+
+public class demo {
+    public demo() {
+    }
+
+    public static void main(String[] args) {
+        int temp1 =11;
+        temp1 = 11;
+        int temp = temp1 - temp1;
+        int temp4 = add(temp1,temp1);
+        System.out.println("fdsfs");
+
+        String name = "";
+
+        if (name == null) {
+
+        }
+
+
+
+
+
+    }
+    public static  Integer add(int temp1, int temp2){
+        int temp3 = temp1 - temp2;
+        return  temp3;
+    }
+}
---
+++ src/com/bwx/idea/pojo.java
@@ -0,0 +1,13 @@
+package com.bwx.idea;
+
+public class pojo {
+    String name = "";
+    String sex = "";
+
+
+    public String getName() {
+        return name;
+    }
+
+
+}
