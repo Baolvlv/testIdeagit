f453d488ebb2ce02a50573e00a7fcad2cfcd1b7b
Baolvlv <baolvlv@gmail.com>
2019-09-15 22:41:35 +0800

    append 2
    Signed-off-by: Baolvlv <baolvlv@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("bwx");
         System.out.println("this is other");
+        System.out.println("fsfdjksfdsjkf");
+        System.out.println("asdhjaaks");
+        System.out.println("dfksjdffjl");
     }
 }
