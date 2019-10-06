845b66c3bd02596aed38d78ddaae699a018d8605
Baolvlv <1027432601@qq.com>
2019-10-06 10:51:19 +0800

    Update pojo.java
    height

--- src/com/bwx/idea/pojo.java
+++ src/com/bwx/idea/pojo.java
@@ -4,7 +4,8 @@ public class pojo {
     String name = "bwx";
     String sex = "man";
     Integer age = 10;
-
+    Integer height = 180;
+    Integer weight = 90;
 
     public String getName() {
         return name;
